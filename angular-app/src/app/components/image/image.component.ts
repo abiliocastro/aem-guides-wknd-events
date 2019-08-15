a582fd9366e48aa49d60915482dee46f90fc929b
abiliocastro <castro.abilio@gmail.com>
2019-08-15 10:01:17 -0300

    Capítulo 6 finished

--- angular-app/src/app/components/image/image.component.ts
+++ angular-app/src/app/components/image/image.component.ts
@@ -8,11 +8,11 @@ import {MapTo} from '@adobe/cq-angular-editable-components';
 })
 export class ImageComponent implements OnInit {
  
-  @Input() src: string
+  @Input() src: string;
   @Input() link: string;
   @Input() alt: string;
   @Input() title: string;
-  @Input() displayPopupTitle: boolean;
+  @Input() displayPopupTitle: string;
  
   constructor() { }
  
@@ -22,6 +22,10 @@ export class ImageComponent implements OnInit {
  
   get imageTitle() {
     return this.displayPopupTitle ? this.title : '';
+  }
+ 
+  get imageCaption() {
+    return this.title;
   }
  
   ngOnInit() { }
